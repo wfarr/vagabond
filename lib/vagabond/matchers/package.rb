20dbfdcb4d0cbc70286c9b623e25cf5560524cc2
Will Farrington <will@railsmachine.com>
2011-09-06 21:42:50 -0400

    better failure messages for package install state

--- lib/vagabond/matchers/package.rb
+++ lib/vagabond/matchers/package.rb
@@ -5,6 +5,14 @@ module Vagabond
         match do |actual|
           actual.state && actual.state =~ /^installed$/
         end
+        
+        failure_message_for_should do |actual|
+          "expected #{actual.to_s} to be installed"
+        end
+        
+        failure_message_for_should_not do |actual|
+          "expected #{actual.to_s} to not be installed"
+        end
       end
     end
   end
